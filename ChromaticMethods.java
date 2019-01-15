edcd4663b5f5674acf3da3087f04cae506b16595
Alain Van Rijn <ayvr015@gmail.com>
2019-01-15 14:34:02 +0100

    some tweaking on the odd cycle finder

--- ChromaticMethods.java
+++ ChromaticMethods.java
@@ -70,7 +70,8 @@ public class ChromaticMethods {
      * to detect odd cycles
      * the starting vertex must have degree > 0 else it will give a false negative
      * <p>
-     * inspired by:
+     * <p>
+     * inspired by: https://www.geeksforgeeks.org/check-graphs-cycle-odd-length/
      *
      * @param adjacencyMatrix the graph
      * @param vertices        mumber of vertices of the graph
@@ -82,29 +83,34 @@ public class ChromaticMethods {
         Arrays.fill(colorArray, -1); // -1 means no color assigned, 1 means the first color, 0 the second.
         // pick a starting vertex
         colorArray[start] = 1; // color with the first color
+
+        // keep track of all vertices so that all components are searched
+        LinkedList allVertices = makeSimpleVerticesList(vertices);
         // create a queue of vertex numbers abd add the starting vertex
         LinkedList<Integer> queue = new LinkedList<Integer>();
         queue.add(start);
 
-        while (!queue.isEmpty()) {
-            // take a vertex from the queue
-            int toCheck = queue.peek();
-            System.out.println("vertex to check is: " + toCheck);
-            queue.pop();
+        while (!allVertices.isEmpty()) {
+            while (!queue.isEmpty()) {
+                // take a vertex from the queue
+                int toCheck = queue.peek();
+                System.out.println("vertex to check is: " + toCheck);
+                queue.pop();
 
-            // find non colored adjacent vertices
-            for (int i = 0; i < vertices; i++) {
-                // whenever and edge from the vertex toCheck to another exists and that vertex is uncolored
-                if (adjacencyMatrix[toCheck][i] && colorArray[i] == -1) {
-                    // the alternate color is assigned to this vertex
-                    colorArray[i] = 1 - colorArray[toCheck];
-                    if (ChromaticSolve.DEBUG)
-                        System.out.println((1 - colorArray[toCheck]) + " assigned to vertex index " + i);
-                    queue.push(i);
+                // find non colored adjacent vertices
+                for (int i = 0; i < vertices; i++) {
+                    // whenever and edge from the vertex toCheck to another exists and that vertex is uncolored
+                    if (adjacencyMatrix[toCheck][i] && colorArray[i] == -1) {
+                        // the alternate color is assigned to this vertex
+                        colorArray[i] = 1 - colorArray[toCheck];
+//                    if (ChromaticSolve.DEBUG)
+//                        System.out.println((1 - colorArray[toCheck]) + " assigned to vertex index " + i);
+                        queue.push(i);
+                    }
+                    // otherwise an edge exists and the same color is used
+                    else if (adjacencyMatrix[toCheck][i] && colorArray[toCheck] == colorArray[i])
+                        return true; // the odd cycle is found
                 }
-                // otherwise an edge exists and the same color is used
-                else if (adjacencyMatrix[toCheck][i] && colorArray[toCheck] == colorArray[i])
-                    return true; // the odd cycle is found
             }
         }
         // the graph is two colorable
